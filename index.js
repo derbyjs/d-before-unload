6e08f06e3a8b3784a9bd6e91f1027c84afe6d106
Nate Smith <nate@nateps.com>
2014-01-06 20:11:05 -0800

    use dom.on method for event listener

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ module.exports = BeforeUnload;
 function BeforeUnload() {}
 BeforeUnload.prototype.view = __dirname;
 
-BeforeUnload.prototype.create = function(model) {
-  function onBeforeUnload(e) {
+BeforeUnload.prototype.create = function(model, dom) {
+  dom.on('beforeunload', window, function(e) {
     if (!model.hasPending()) return;
     var confirmationMessage = model.get('message') ||
       'You have unsaved changes. Do you want to leave this page and discard your changes?';
     e.returnValue = confirmationMessage;
     return confirmationMessage;
-  }
-  if (window.addEventListener) {
-    window.addEventListener('beforeunload', onBeforeUnload, false);
-  } else {
-    window.onbeforeunload = onBeforeUnload;
-  }
+  });
 };
