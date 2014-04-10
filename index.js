57970fac4ebe11639484092176db635d0129172e
Nate Smith <nate@nateps.com>
2014-04-09 20:11:28 -0700

    don't block leaving page on initial subscriptions
    wait for the user to make some sort of mutation before adding the
    before unload listener

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ function BeforeUnload() {}
 BeforeUnload.prototype.name = 'd-before-unload';
 
 BeforeUnload.prototype.create = function(model, dom) {
-  dom.on('beforeunload', window, function(e) {
+  // Don't do anything until the user makes some kind of mutation
+  model.root.once('all', '**', function() {
+    // Thereafter, stop the user from leaving the page whenever a change might be pending
+    dom.on('beforeunload', window, listenerFn(model));
+  });
+};
+
+function listenerFn(model) {
+  return function beforeunloadListener(e) {
     if (!model.hasPending()) return;
     var confirmationMessage = model.get('message') ||
       'You have unsaved changes. Do you want to leave this page and discard your changes?';
     e.returnValue = confirmationMessage;
     return confirmationMessage;
-  });
-};
+  };
+}
