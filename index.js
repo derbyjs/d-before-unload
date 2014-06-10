4fd83d604d1c66cd1a579f9843d64dba895b0d51
Nate Smith <nate@nateps.com>
2014-06-09 22:39:10 -0700

    use new model.hasWritePending instead of model.hasPending

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ function BeforeUnload() {}
 BeforeUnload.prototype.name = 'd-before-unload';
 
 BeforeUnload.prototype.create = function(model, dom) {
-  // Don't do anything until the user makes some kind of mutation
-  model.root.once('all', '**', function() {
-    // Thereafter, stop the user from leaving the page whenever a change might be pending
-    dom.on('beforeunload', window, listenerFn(model));
-  });
+  // Stop the user from leaving the page whenever a change might be pending
+  dom.on('beforeunload', window, listenerFn(model));
 };
 
 function listenerFn(model) {
   return function beforeunloadListener(e) {
-    if (!model.hasPending()) return;
+    if (!model.hasWritePending()) return;
     var confirmationMessage = model.get('message') ||
       'You have unsaved changes. Do you want to leave this page and discard your changes?';
     e.returnValue = confirmationMessage;
